3b4a09cbb707dd56d086b4b0ffca4f9736908f68
Judith Olivia <jxdith13@gmail.com>
2025-10-05 19:00:54 -0400

    frozenset

--- volume_control.py
+++ volume_control.py
@@ -1,3 +1,14 @@
-import cv2
+from cv2 import cv2
 import mediapipe as mp
+import time, math
 import pyautogui
+
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+HandLandmark = mp_hands.HandLandmark
+
+
+hconnect = frozenset([
+    (HandLandmark.WRIST, HandLandmark.THUMB_CMC),
+    
+])
