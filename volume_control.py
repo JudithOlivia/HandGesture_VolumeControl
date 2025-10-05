9de441e806d8f4464dda0dc11288357de2be50bc
Judith Olivia <jxdith13@gmail.com>
2025-10-05 19:01:56 -0400

    hand parts

--- volume_control.py
+++ volume_control.py
@@ -10,5 +10,24 @@ HandLandmark = mp_hands.HandLandmark
 
 hconnect = frozenset([
     (HandLandmark.WRIST, HandLandmark.THUMB_CMC),
-    
+    (HandLandmark.THUMB_CMC, HandLandmark.THUMB_MCP),
+    (HandLandmark.THUMB_MCP, HandLandmark.THUMB_IP),
+    (HandLandmark.THUMB_IP, HandLandmark.THUMB_TIP),
+    (HandLandmark.WRIST, HandLandmark.INDEX_FINGER_MCP),
+    (HandLandmark.INDEX_FINGER_MCP, HandLandmark.INDEX_FINGER_PIP),
+    (HandLandmark.INDEX_FINGER_PIP, HandLandmark.INDEX_FINGER_DIP),
+    (HandLandmark.INDEX_FINGER_DIP, HandLandmark.INDEX_FINGER_TIP),
+    (HandLandmark.INDEX_FINGER_MCP, HandLandmark.MIDDLE_FINGER_MCP),
+    (HandLandmark.MIDDLE_FINGER_MCP, HandLandmark.MIDDLE_FINGER_PIP),
+    (HandLandmark.MIDDLE_FINGER_PIP, HandLandmark.MIDDLE_FINGER_DIP),
+    (HandLandmark.MIDDLE_FINGER_DIP, HandLandmark.MIDDLE_FINGER_TIP),
+    (HandLandmark.MIDDLE_FINGER_MCP, HandLandmark.RING_FINGER_MCP),
+    (HandLandmark.RING_FINGER_MCP, HandLandmark.RING_FINGER_PIP),
+    (HandLandmark.RING_FINGER_PIP, HandLandmark.RING_FINGER_DIP),
+    (HandLandmark.RING_FINGER_DIP, HandLandmark.RING_FINGER_TIP),
+    (HandLandmark.RING_FINGER_MCP, HandLandmark.PINKY_MCP),
+    (HandLandmark.WRIST, HandLandmark.PINKY_MCP),
+    (HandLandmark.PINKY_MCP, HandLandmark.PINKY_PIP),
+    (HandLandmark.PINKY_PIP, HandLandmark.PINKY_DIP),
+    (HandLandmark.PINKY_DIP, HandLandmark.PINKY_TIP)
 ])
